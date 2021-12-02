cc105a69f41f327ddd33424fdd88e87f38ae5a02
Conejo021 <gustavoconiglio@gmail.com>
2021-12-02 04:03:38 -0300

    Se sube la version Final del Desafio

--- APIsRest/controllers/api/generosApiController.js
+++ APIsRest/controllers/api/generosApiController.js
@@ -3,10 +3,7 @@ const db = require('../../database/models');
 module.exports = {
     list: (req,res) => {
         db.Genero.findAll()
-       // include:[
-          // {association:"canciones"},
-            //       ]
-
+      
         .then(generos => {
             let respuesta = {
                 meta: {
@@ -19,6 +16,23 @@ module.exports = {
 
            return res.json(respuesta)
         }).catch(error=>res.send(error))
-    }
+    },
+    generosCanciones: (req, res) => {
+        //const id=req.params.id;
+        db.Genero.findByPk(req.params.id,{
+            include: ['canciones']
+        })
+            .then(generos => {
+                let respuesta = {
+                    meta: {
+                        status: 200,
+                        total: generos.length,
+                        url: '/api/generos/:id/canciones'
+                    },
+                    data: generos
+                }
+                return res.json(respuesta);
+            }).catch(error=>res.send(error))
+    },
 }
 //module.exports = generosApiController;
--- APIsRest/routes/api/generosApiRouter.js
+++ APIsRest/routes/api/generosApiRouter.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const generosApiController = require('../../controllers/api/generosApiController');
 
 router.get('/', generosApiController.list);
+router.get('/:id/canciones', generosApiController.generosCanciones);
 
 
 
